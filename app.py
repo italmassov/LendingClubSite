cc1da6b0b72b8edb16b53a5f9330296235a6e79e
Kuanysh Italmassov <italmas2010@gmail.com>
2015-09-09 15:49:32 -0500

    lighter version

--- app.py
+++ app.py
@@ -2,29 +2,8 @@ from flask import Flask, render_template, request, redirect
 import flask
 import requests
 import datetime
-import pandas as pd
-import numpy as np
-
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.templates import RESOURCES
-from bokeh.utils import encode_utf8
 
 app = Flask(__name__)
-
-colors = {
-    'Black': '#000000',
-    'Red': '#FF0000',
-    'Green': '#00FF00',
-    'Blue': '#00FF00'
-}
-
-dates = 0
-closing_prices = 0
-
-# stock ticker
-st = ''
 
 def getitem(obj, item, default):
     if item not in obj:
